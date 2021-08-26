9bd5c3ed6aec7184519f9065e24c62b1ada7ac73
Serge <Kalmikkovw13@gmail.com>
2021-08-25 19:23:24 -0500

    v1.7.1

---
+++ Homework1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Homework1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var o = new Service();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
